4eacef48e2f96428a40ccef1176b3ceeaa0ebf88
Smak2025 <smak-80+2025@yandex.ru>
2026-02-26 17:24:08 +0300

    26.02.26

--- src/edu/gr05513/Main.kt
+++ src/edu/gr05513/Main.kt
@@ -1,2 +1,32 @@
 package edu.gr05513
 
+import edu.gr05513.math.Complex
+
+fun main() {
+    val z1 = Complex(1.0, 2.0) // 1 + 2i
+    val z2 = Complex(2.0, -3.0) // 2 - 3i
+    val z3 = Complex(3.0) // 3
+    val z4 = Complex(im = -1.0) // -i
+    val z5 = Complex(im = 1.0) // i
+    val z6 = Complex(im = 2.0) // 2i
+    println(z1)
+    println(z2)
+    println(z3)
+    println(z4)
+    println(z5)
+    println(z6)
+
+    println(z1 + z2)
+    println(z1 - z2)
+    println(z1 * z2)
+    // TODO: Добавить
+    // println(z1 / z2) (div)
+    // z1 += z2 (plusAssign)
+    // z1 -= z2
+    // z1 *= z2
+    // z1 /= z2
+
+    // z1.abs() (нахождение модуля комплексного числа)
+    // z1.arg() (аргумент комплексного числа)
+    // z1.conjugate() (сопряженное комплексное число)
+}
--- src/edu/gr05513/math/Complex.kt
+++ src/edu/gr05513/math/Complex.kt
@@ -1,4 +1,42 @@
 package edu.gr05513.math
 
-class Complex {
+import kotlin.math.absoluteValue
+
+// re + i * im
+class Complex(
+    var re: Double = 0.0,
+    var im: Double = 0.0,
+) {
+
+    operator fun plus(other: Complex): Complex {
+        return Complex(this.re + other.re, im + other.im)
+    }
+
+    operator fun minus(other: Complex): Complex {
+        return Complex(this.re - other.re, im - other.im)
+    }
+
+    // z1 * z2 = (a + b i)(c + d i) = ac + adi + bci - bd = (ac - bd) + (ad + bc)i
+    operator fun times(other: Complex): Complex {
+        return Complex(
+            this.re * other.re - this.im * other.im,
+            this.re * other.im + this.im * other.re,
+        )
+    }
+
+    override fun toString(): String = buildString{
+        if (re != 0.0 || im == 0.0) {
+            append(re)
+        }
+        if (im != 0.0){
+            if (im > 0.0) {
+                if (re != 0.0) append("+")
+            }
+            else append("-")
+            if (im.absoluteValue != 1.0) {
+                append(im.absoluteValue)
+            }
+            append("i")
+        }
+    }
 }
